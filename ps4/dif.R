c94b6fd91823df2bb7b37d37d2e4736732f29b53
Klint Kanopka <klint.kanopka@gmail.com>
2018-03-04 21:44:19 -0800

    dif

---
+++ ps4/dif.R
@@ -0,0 +1,31 @@
+resp <-read.table("emp-3pl-math-reading.txt",header=TRUE)
+resp
+readsums <- rowSums(resp[,47:102])
+readsums
+resp[order(rowSums(resp[,47:102])),]->resp2
+resp2
+mathsums <- rowSums(resp2[,2:46])
+mathsums
+member <- c(rep(0,731),rep(1,731))
+member
+
+
+item <- matrix(NA,45,2)
+for (i in 2:46){
+  m <- glm(resp2[,i] ~ mathsums + member, family='binomial')
+  obj<-summary(m)
+  item[i-1,1] <- obj$coefficients[3,1]
+  item[i-1,2] <- obj$coefficients[3,4]
+}
+item
+plot(2:46,item[,2])
+abline(h=0.05,col='red')
+
+
+plot(2:46,item[,1])
+abline(h=0.05,col='red')
+hist(item[,1])
+
+plot(2:46,item[,1],col=ifelse(item[,2]<=0.05,'red','blue'))
+hist(item[,2])
+plot(item[,1],item[,2])
